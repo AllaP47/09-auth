c3fe63db9954494965731c54ebff350477ef93ff
AllaP47 <allapaschinska1@gmail.com>
2026-08-08 19:29:24 +0300

    add message

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL
   ? `${process.env.NEXT_PUBLIC_API_URL}/api`
-  : 'https://goit.study';
+  : 'https://notehub-api.goit.study';
 
 export const api = axios.create({
   baseURL,
